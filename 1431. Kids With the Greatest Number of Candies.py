8eb49a9fd7e16554d0349a2ca8084fb39f324452
Yonas Enchalew <yonasenchalew@gmail.com>
2023-05-21 06:00:13 +0300

    first

---
+++ 1431. Kids With the Greatest Number of Candies.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandies = max(candies)
+        result = [False] * len(candies)
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= maxCandies:
+                result[i] = True
+        return result
