a9653197765c769ae693f065d8e2fd24f6436d4f
Yonas Enchalew <yonasenchalew@gmail.com>
2023-12-16 09:44:07 +0300

    a

--- tutorial/bubbleSort.py
+++ tutorial/bubbleSort.py
@@ -1,14 +1,13 @@
-def bubbleSort(list):
+def bubble_sort(list):
     unsorted_index = len(list) - 1
     sorted = False
 
     while not sorted:
         sorted = True
         for i in range(unsorted_index):
-            if list[i]>list[i+1]:
+            if list[i] > list[i+1]:
                 list[i], list[i+1] = list[i+1], list[i]
                 sorted = False
-        unsorted_index -=1
+        unsorted_index -= 1
     return list
-    
-print(bubbleSort([65,55,45,35,25,10]))
+print(bubble_sort([1,6,4,5,74,3]))
