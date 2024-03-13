1d8fb600f17652cd42428875a3c8146e0b4589b8
Yonas Enchalew <88850442+Yonas1219@users.noreply.github.com>
2024-03-13 17:29:55 +0300

    Update 1720. Decode XORed Array.py

--- 1720. Decode XORed Array.py
+++ 1720. Decode XORed Array.py
@@ -1,6 +1,6 @@
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
-        arr = [first]  # Initialize arr with the first element
+        arr = [first]  # Initialization 
 
         for i in range(len(encoded)):
             next_element = arr[i] ^ encoded[i]  # XOR operation to find the next element
