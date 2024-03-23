210944b9b540319eeb670cd2fea2d53071243f05
Yonas Enchalew <yonasenchalew@gmail.com>
2024-03-23 10:16:42 +0300

    changes

--- 1480. Running Sum of 1d Array.py
+++ 1480. Running Sum of 1d Array.py
@@ -2,7 +2,7 @@ class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         running_sum = 0
         result = []
-
+        #Initialization
         for num in nums:
             running_sum += num
             result.append(running_sum)
