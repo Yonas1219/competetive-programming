baa476550a77a23c18c8b5684f778e570d0a46b8
Yonas Enchalew <yonasenchalew@gmail.com>
2024-03-30 10:22:15 +0300

    changes

--- 1920.Build Array from Permutation.py
+++ 1920.Build Array from Permutation.py
@@ -2,7 +2,7 @@ class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
        
         n = len(nums)
-        val = 0
+        val = 0 # intilaze value
         for i in range(0, n):
             nums[i] = n * ( nums[nums[i]] % n) + nums[i]
         for i in range(0,n):
